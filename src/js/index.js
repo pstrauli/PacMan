eeb0c667d52240466ce36d1d59052abd7564b4d9
Peter <56368462+pstrauli@users.noreply.github.com>
2020-02-07 09:54:42 +0100

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // console.log('it works');
+
+  document.addEventListener('click', () => {
+    const pacChomp = document.querySelector('#')
+  })
+
+
+
+
+});
