77e39537df6fcc05e30dbd1a3b506977a472d676
Peter <56368462+pstrauli@users.noreply.github.com>
2020-02-07 10:05:01 +0100

    Created selector for PacMan

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
   // console.log('it works');
 
   document.addEventListener('click', () => {
-    const pacChomp = document.querySelector('#')
+    const pacManElm = document.querySelector('.entity--pac');
+    pacManElm.style.backgroundPositionX = `85px`;
   })
 
 
