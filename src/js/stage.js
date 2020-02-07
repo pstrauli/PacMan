8a1037d17dd4b9b49f17587c2aace963f50ffe8f
Petr Bělecký <23419559+PetrBelecky@users.noreply.github.com>
2020-02-07 15:31:48 +0100

    generated more wall

--- src/js/stage.js
+++ src/js/stage.js
@@ -38,18 +38,48 @@ class Stage {
     const wall = new Entity(0, 1, 'wall');
     const apple = new Entity(1, 2, 'apple');
     const bomb = new Entity(1, 0, 'bomb');
-    const wall2 = new Entity(2, 1, 'apple');
+    const wall2 = new Entity(2, 1, 'wall');
+    const wall3 = new Entity(3, 2, 'wall');
+    const wall4 = new Entity(3, 3, 'wall');
+    const wall5 = new Entity(3, 4, 'wall');
+    const wall6 = new Entity(3, 5, 'wall');
+    const wall7 = new Entity(3, 1, 'wall');
+    const wall8 = new Entity(2, 1, 'wall');
+    const wall9 = new Entity(2, 2, 'wall');
+    const wall10 = new Entity(2, 5, 'wall');
+    const wall11 = new Entity(0, 1, 'wall');
+    const wall12 = new Entity(0, 2, 'wall');
 
     pacman.mount(this.element);
     wall.mount(this.element);
     apple.mount(this.element);
     bomb.mount(this.element);
     wall2.mount(this.element);
+    wall3.mount(this.element);
+    wall4.mount(this.element);
+    wall5.mount(this.element);
+    wall6.mount(this.element);
+    wall7.mount(this.element);
+    wall8.mount(this.element);
+    wall9.mount(this.element);
+    wall10.mount(this.element);
+    wall11.mount(this.element);
+    wall12.mount(this.element);
 
     this.entities.push(wall);
     this.entities.push(apple);
     this.entities.push(bomb);
     this.entities.push(wall2);
+    this.entities.push(wall3);
+    this.entities.push(wall4);
+    this.entities.push(wall5);
+    this.entities.push(wall6);
+    this.entities.push(wall7);
+    this.entities.push(wall8);
+    this.entities.push(wall9);
+    this.entities.push(wall10);
+    this.entities.push(wall11);
+    this.entities.push(wall12);
 
     console.log(pacman);
 
