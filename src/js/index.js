cbba910ce37e4eea5e784f6e441e2ce0e22d0521
Peter <56368462+pstrauli@users.noreply.github.com>
2020-02-07 10:14:49 +0100

    PacMan moves on ArrowRight

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,26 @@
+let xpos = 0;
+const TILE_SIZE = 85;
+
 document.addEventListener('DOMContentLoaded', () => {
   // console.log('it works');
 
+  const pacManElm = document.querySelector('.entity--pac');
+
   document.addEventListener('click', () => {
-    const pacManElm = document.querySelector('.entity--pac');
+    
     pacManElm.style.backgroundPositionX = `85px`;
   })
+
+  document.addEventListener('keydown', (event) => {
+
+    if(event.key === 'ArrowRight') {
+      pacManElm.style.left = `${TILE_SIZE + xpos}px`;
+      xpos += TILE_SIZE;
+      
+  
+    }
+  
+  });
 
 
 
